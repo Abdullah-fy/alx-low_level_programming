e04836496dc8e6eb41b7af60a113ade68b61cbbd
Abdullah-fy <abdullahfathy2023@gmail.com>
2023-02-17 00:25:14 -0800

    commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,13 +6,9 @@
  */
 int main(void)
 {
-	int i;
-	float f;
-	long int l;
-	char c;
-	printf("size of char: %lu byte(s)\n", sizeof(char));
-	printf("size of long int: %lu byte(s)\n", sizeof(long int));
-	printf("size of float: %lu byte(s)\n", sizeof(float));
-	printf("size of int: %lu byte(s)\n", sizeof(int));
+	printf("Size of a char: %lu byte(s)\n", sizeof(char));
+	printf("Size of a int: %lu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+	printf("Size of a float: %lu byte(s)\n", sizeof(float));
 	return (0);
 }
