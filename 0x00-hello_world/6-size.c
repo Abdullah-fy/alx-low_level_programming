7d9551a7d1679941a63a04dd0468291bdf1d5058
Abdullah-fy <abdullahfathy2023@gmail.com>
2023-02-16 19:57:03 -0800

    all

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/*main point starts*/
+int main(void)
+{            
+
+	int i;
+	char c;
+	long int l;
+	float f;
+	printf("size of a char: %lu byte(s)\n", sizeof(char));
+	 printf("size of a int: %lu byte(s)\n", sizeof(int));
+	  printf("size of a long int: %lu byte(s)\n", sizeof(long int));
+	   printf("size of a float: %lu byte(s)\n", sizeof(float));
+	   return(0);
+}
+
+
