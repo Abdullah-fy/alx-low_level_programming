641c91c0f2f0b83ee6cb82d259d2a9520c1ad64c
Abdullah-fy <abdullahfathy2023@gmail.com>
2023-02-18 08:54:01 -0800

    d

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * Description: prints numbers, 
+ * Description: prints numbers,
  * Return: Always 0 (success)
  */
 int main(void)
 {
 	int a;
 
-	for (a = '0'; a < '9'; a++)
+	for (a = '0'; a <= '9'; a++)
 	{
 		putchar(a);
 		if (a != 9)
