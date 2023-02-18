e54333543e78c29d4e1a0f53429f208d86cb9f0f
Abdullah-fy <abdullahfathy2023@gmail.com>
2023-02-18 06:47:47 -0800

    f

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,4 +1,4 @@
-#include <stdio.c>
+#include <stdio.h>
 /**
  * main - entry point
  * Description : print all single digite from 0 to 10
@@ -8,6 +8,6 @@ int main(void)
 {
 	for (int n = 0; n < 10; n++)
 	{
-		prinf("%d \n",n );
+		printf("%d \n",n );
 	}
 }
