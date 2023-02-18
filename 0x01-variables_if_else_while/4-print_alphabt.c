4ba7e4ea52503a3f165b681a0cba6cb24829b27c
Abdullah-fy <abdullahfathy2023@gmail.com>
2023-02-18 06:38:48 -0800

    d
    git push origin master

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: prints alphabet in lowercase expect e and q
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char low;
+	for (low = 'a'; low <= 'z'; low++)
+	{
+		if (low != 'e' && low != 'q')
+		{
+			putcher(low);
+		}
+		putcher(\n);
+	{
+}
