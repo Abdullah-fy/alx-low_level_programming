9637adc1b691efa43bd0eaae382f2c37dd23d23c
Abdullah-fy <abdullahfathy2023@gmail.com>
2023-02-21 09:19:26 -0800

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int _islower(int c)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		if (i == c)
+		{
+			return (1);
+		}
+		else 
+		{
+			return (0);
+		}
+	}
+}	
