377c71c4e7de910846e4fb99fae88b791b9d5422
Abdullah-fy <abdullahfathy2023@gmail.com>
2023-02-21 07:26:39 -0800

    #include main.h
    /**
     * print_alphabet - function that prints alphabet in lowercase
     *
     * Return: Always 0
     */
    void print_alphabet(void)
    {
    int alpha;
    
    for (alpha = 'a'; alpha <= 'z'; alpha++)
    {
    _putchar(alpha);
    }
    _putchar('\n');

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,17 @@
-#include <stdio.h>
+#include "main.h"
+
 /**
- * main - check the code
+ * print_alphabet - function that prints alphabet in lowercase 
  *
- * Return: Always 0.
+ * Return: Always 0 
  */
 void print_alphabet(void)
 {
-	char i;
+	int alpha;
 
-	for (i = 'a'; i <= 'z'; i++)
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
 	{
-		putchar(i);
+		_putchar(alpha);
 	}
-	putchar ('\n');
-	return (0);
+	_putchar('\n');
 }
