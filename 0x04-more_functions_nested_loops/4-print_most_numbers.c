cce3c7126a37e0ff49712d69e407330f0d80f2d6
Abdullah-fy <abdullahfathy2023@gmail.com>
2023-02-23 03:42:50 -0800

    ad'

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,11 +8,12 @@ void print_most_numbers(void)
 {
 	char c;
 
-	for (c = '0'; c <= '9')
+	for (c = '0'; c <= '9';)
 	{
 		if (c != '2' && c != '4')
 		{
 			_putchar(c);
 		}
+	}
 	_putchar('\n');
 }
