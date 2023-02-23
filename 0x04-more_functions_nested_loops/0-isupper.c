362aac606c7befa58b961ebf561f8d1961d5f14c
Abdullah-fy <abdullahfathy2023@gmail.com>
2023-02-23 01:41:03 -0800

    isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * isupper - checks is a character is uppercase
+ * @c : intiger variable
+ * return 1 if true and 0 if false
+ */
+int _isupper(int c)
+{
+	if (c = 'C')
+		return (1);
+	else 
+		retrun (0);
+}
