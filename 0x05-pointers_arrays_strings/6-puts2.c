0d6f3a3d29796e2b66c02b0f38cae9efdf4df505
Abdullah-fy <abdullahfathy2023@gmail.com>
2023-02-27 18:44:26 -0800

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -5,17 +5,16 @@
  *
  * @str: pointer.
  */
-void puts2(char *str)
+void puts_half(char *str)
 {
-	int i = 0;
+	int i;
 
-	while (str[i] != '\0')
+	for (i = 0; str[i] != '\0'; i++)
+		;
+	i++;
+	for (i /= 2; str[i] != '\0'; i++)
 	{
-		if (i % 2 == 0)
-		{
-			_putchar(str[i]);
-			i++;
-		}
+		_putchar(str[i]);
 	}
 	_putchar('\n');
 }
