c93a6cf328ff830201f062254dbd46c9d327f714
Abdullah-fy <https://github.com/Abdullah-fy/alx-low_level_programming.git>
2023-03-01 14:40:06 -0800

    d

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -6,24 +6,37 @@
  *
  * Return: string
  */
-char *cap_string(char *s)
+char *cap_string(char *n)
 {
-int i, j;
-char prev = ' ';
+	int i;
 
-for (i = 0; s[i] != '\0'; i++)
-{
-if (prev == ' ' || prev == '\t' || prev == '\n' ||
-prev == ',' || prev == ';' || prev == '.' ||
-prev == '!' || prev == '?' || prev == '"' ||
-prev == '(' || prev == ')' || prev == '{' || prev == '}')
-{
-if (s[i] >= 'a' && s[i] <= 'z')
-s[i] = s[i] - 'a' + 'A';
-}
-
-prev = s[i];
-}
-
-return (s);
+	i = 0;
+	if (n[0] >= 'a' && n[0] <= 'z')
+	{
+		n[0] = n[0] - 32;
+	}
+	for (i = 0; n[i] != '\0'; i++)
+	{
+		switch (n[i])
+		{
+			case ',':
+			case ';':
+			case '.':
+			case '!':
+			case '?':
+			case '"':
+			case '(':
+			case ')':
+			case '{':
+			case '}':
+			case ' ':
+			case '\n':
+			case '\t':
+				if (n[i + 1] > 96 && n[i + 1] < 123)
+				{
+					n[i + 1] = n[i + 1] - 32;
+				}
+		}
+	}
+	return (n);
 }
