302fc38e20e35c15c54893a3c0bd72f3d8cab5e3
Abdullah-fy <https://github.com/Abdullah-fy/alx-low_level_programming.git>
2023-03-01 13:42:44 -0800

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,23 +8,16 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0;
+	int i;
 
-	while (s1[i] != '\0' && s2[i] != '\0' && s1[i] == s2[i])
+	i = 0;
+	while (s1[i] != '\0' && s2[i] != '\0')
 	{
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
 		i++;
 	}
-
-	if (s1[i] == s2[i])
-	{
-		return (0);
-	}
-	else if (s1[i] < s2[i])
-	{
-		return (-15);
-	}
-	else
-	{
-		return (15);
-	}
+	return (0);
 }
