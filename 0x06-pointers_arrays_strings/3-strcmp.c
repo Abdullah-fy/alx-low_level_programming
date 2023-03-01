84f02d2d6c39829b1cf78ddc9495c0c57ca7afaa
Abdullah-fy <https://github.com/Abdullah-fy/alx-low_level_programming.git>
2023-03-01 13:28:32 -0800

    3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * _strcmp - compare two strings
+ * @s1: the first string
+ * @s2: the second string
+ *
+ * Return: an integer less than, equal to, or greater than zero.
+ */
+int _strcmp(char *s1, char *s2)
+{
+    int i = 0;
+
+    while (s1[i] != '\0' && s2[i] != '\0' && s1[i] == s2[i])
+    {
+        i++;
+    }
+
+    if (s1[i] == s2[i])
+    {
+        return 0;
+    }
+    else if (s1[i] < s2[i])
+    {
+        return -1;
+    }
+    else
+    {
+        return 1;
+    }
+}
+
