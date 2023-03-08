8ad9e031cc75d6e383d2f656068cfe143ecebe13
Abdullah-fy <https://github.com/Abdullah-fy/alx-low_level_programming.git>
2023-03-08 14:21:05 -0800

    0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _puts_recursion - Prints a string, followed by a new line
+ *
+ * @s: A pointer to the string to be printed
+ *
+ * Return: void
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+		_putchar('\0');
+	else
+		_putchar(*s);
+		_puts_recursion(s + 1);
+}
