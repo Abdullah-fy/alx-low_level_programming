b1d8dbfefd03bb615dfa3ce2335004cd137f9ddd
Abdullah-fy <https://github.com/Abdullah-fy/alx-low_level_programming.git>
2023-03-10 04:51:55 -0800

    whatsmyname

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - prints the program name
+ * @argc: the number of command-line arguments
+ * @argv: an array of strings containing the command-line arguments
+ *
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", *argv);
+	return (0);
+}
