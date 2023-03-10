ac406b1577c00540dfe28465be07e45cec348e74
Abdullah-fy <https://github.com/Abdullah-fy/alx-low_level_programming.git>
2023-03-10 06:24:38 -0800

    3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -17,8 +17,8 @@ int main(int argc, char *argv[])
 	}
 	else
 	{
-		n1 = atio(argv[1]);
-		n2 = atio(argv[2]);
+		n1 = atoi(argv[1]);
+		n2 = atoi(argv[2]);
 		printf("%d\n", n1 * n2);
 	}
 	return (0);
