f3aa55d0a6a2b82e85bae6d4e4cf4ecbcd7383e8
Abdullah-fy <https://github.com/Abdullah-fy/alx-low_level_programming.git>
2023-03-13 16:16:03 -0700

    0. Float like a butterfly, sting like a bee

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * create_array - A function that creates an array of chars
+ * and initializes it with a specific char.
+ *
+ * @size: size of array.
+ * @c: char to initialize the array with.
+ *
+ * Return: Pointer to the array, or NULL if it fails.
+ **/
+char *create_array(unsigned int size, char c)
+{
+	int i;
+	char *str;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	str = (int *)malloc(size * sizeof(char));
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	while (i < size)
+	{
+		str[i] = c;
+		i++;
+	}
+	return (str);
+}
