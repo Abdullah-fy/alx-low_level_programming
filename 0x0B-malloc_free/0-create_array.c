25b6a78c1b61733419be93893a300fa18eab827e
Abdullah-fy <https://github.com/Abdullah-fy/alx-low_level_programming.git>
2023-03-13 16:24:44 -0700

    0. Float like a butterfly, sting like a bee

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,18 +11,15 @@
  **/
 char *create_array(unsigned int size, char c)
 {
-	unsigned int i;
 	char *str;
+	unsigned int i;
 
 	if (size == 0)
-	{
 		return (NULL);
-	}
-	str = (char *)malloc(size * sizeof(char));
+	str = malloc(sizeof(char) * size);
 	if (str == NULL)
-	{
 		return (NULL);
-	}
+	i = 0;
 	while (i < size)
 	{
 		str[i] = c;
