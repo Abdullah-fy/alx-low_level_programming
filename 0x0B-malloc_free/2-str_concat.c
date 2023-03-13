5833498baa01fbc4bbd4651cdccabac7722dd38c
Abdullah-fy <https://github.com/Abdullah-fy/alx-low_level_programming.git>
2023-03-13 16:58:20 -0700

    fix betty

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -23,7 +23,7 @@ char *str_concat(char *s1, char *s2)
 	for (index = 0; s1[index] || s2[index]; index++)
 		i++;
 
-	concat_str =(char *)malloc(sizeof(char) * i);
+	concat_str = (char *)malloc(sizeof(char) * i);
 
 	if (concat_str == NULL)
 		return (NULL);
