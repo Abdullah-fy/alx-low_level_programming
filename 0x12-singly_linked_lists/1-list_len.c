0560ea3fb0937e94180df76dda49a42f736af3c2
Abdullah-fy <https://github.com/Abdullah-fy/alx-low_level_programming.git>
2023-03-31 19:03:29 -0700

    1-list_len.c

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,21 +1,18 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "lists.h"
 
 /**
-  * list_len - Show the number of elements of a list
-  * @h: A linked list
-  *
-  * Return: The number of elements of a list
-  */
+ * list_len - returns the number of elements in a linked list_t list
+ * @h: head of the linked list
+ * Return: number of elements in the list
+ */
 size_t list_len(const list_t *h)
 {
 	size_t count = 0;
 
-	while (h)
+	while (h != NULL)
 	{
-		h = h->next;
 		count++;
+		h = h->next;
 	}
 
 	return (count);
