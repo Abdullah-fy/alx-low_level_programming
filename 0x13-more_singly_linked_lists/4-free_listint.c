6c50c1f625dafc57fa027485361baac4a0e18d9f
Abdullah-fy <https://github.com/Abdullah-fy/alx-low_level_programming.git>
2023-04-03 09:49:59 -0700

    4-free_listint.c

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_listint -  frees a listint_t list
+ *
+ * @head: pointer to the head
+ *
+ * Return: nothing
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *this_node;
+
+	while (head)
+	{
+		this_node = *head;
+		head = head->next;
+		free(this_node);
+	}
+}
