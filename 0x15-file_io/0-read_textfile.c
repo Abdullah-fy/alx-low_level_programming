a08250a58a097532fc7aeb7f79bb05f14562e631
Abdullah-fy <https://github.com/Abdullah-fy/alx-low_level_programming.git>
2023-04-10 08:49:07 -0700

    fix

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * read_textfile - reads a text file and prints it to the POSIX standard output
  *
@@ -8,45 +7,39 @@
  *
  * Return: the actual number of letters it could read and print
  */
-
 ssize_t read_textfile(const char *filename, size_t letters)
 {
+	int fp, num_bytes_read, num_bytes_written;
+	char *buffer;
+
 	if (filename == NULL)
 		return (0);
 
-	int fp = open(filename, O_RDONLY);
-
+	fp = open(filename, O_RDONLY);
 	if (fp == -1)
 	{
 		return (0);
 	}
-
-	char *buffer = malloc(sizeof(char) * letters);
-
+	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
 	{
 		close(fp);
 		return (0);
 	}
-
-	int num_bytes_read = read(fp, buffer, letters);
-
+	num_bytes_read = read(fp, buffer, letters);
 	if (num_bytes_read == -1)
 	{
 		close(fp);
 		free(buffer);
 		return (0);
 	}
-
-	int num_bytes_written = write(STDOUT_FILENO, buffer, num_bytes_read);
-
+	num_bytes_written = write(STDOUT_FILENO, buffer, num_bytes_read);
 	if (num_bytes_written == -1 || num_bytes_written != num_bytes_read)
 	{
 		close(fp);
 		free(buffer);
 		return (0);
 	}
-
 	free(buffer);
 	close(fp);
 	return (num_bytes_written);
