461370e53d8c0f94c5c877079016147765ccdbf2
Abdullah-fy <https://github.com/Abdullah-fy/alx-low_level_programming.git>
2023-04-10 15:12:27 -0700

    fix:

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -8,6 +8,11 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+void check_arguments(int argc, char **argv);
+FILE* open_file(const char* filename, const char* mode);
+void close_file(FILE* fp, const char* filename);
+void copy_file(FILE* fp_from, FILE* fp_to);
+
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
